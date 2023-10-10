8657600c038dad11b336f81a3dd61736b362251d
lilyjohnston <ljohnston802@gmail.com>
2023-10-10 16:21:10 -0600

    updated file so that it's the same as bash script on RPi

--- data_collection.sh
+++ data_collection.sh
@@ -24,6 +24,26 @@ do
 done
 }
 
+PWR_STAT=0
+exitscript(){
+	echo"$(date): SIGINT or SIGTERM detected"
+	trap -SIGINT SIGTERM
+	if ! [-z ${CHILD+x}]
+	then
+		echo "Terminating data logging"
+		kill $CHILD
+		wait $CHILD
+	fi
+	blink()
+	if [ $PWR_STAT -eq 1]
+	then
+		echo "powering off computer"
+#poweroff
+	fi
+	exit 1
+}
+trap exitscript SIGINT SIGTERM
+
 dt=$(date '+%Y_%m_%d_%H_%M')
 umask 0111
 touch $dt.pcap
@@ -32,7 +52,6 @@ lightoff
 sleep 5
 
 # Main script
-if eth_active; then
 # Ethernet active, execute the command
     dumpcap -i eth0 -P -w $dt.pcap &
     pid=$!
@@ -40,7 +59,7 @@ if eth_active; then
         echo "working"
         sleep 2
     else
-        kill -2 $pid
+        kill -9 $pid
         echo "Ethernet interface is severed. Exiting..."
         blink
         shutdown -h now
