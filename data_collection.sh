e2b80f3d6d4129cf7b8cd4e4d302c474a28c1931
lilyjohnston <ljohnston802@gmail.com>
2023-10-10 16:44:53 -0600

    stack overflow code

--- data_collection.sh
+++ data_collection.sh
@@ -73,3 +73,44 @@ fi
 #fflush CMD
 #Check fstab for write permissions
 #a test we can do is edit rc local is echo "hi">~/blank.txt
+
+#TEST FOR EXITING SCRIPT WHEN ETHERNET IS UNPLUGGED
+if eth_active; then
+	dumpcap -i eth0 -P -w "$dt.pcap" &
+    	pid=$!
+else
+    # Ethernet interface is severed, exit the script
+    echo "Not getting data"
+    blink
+    exit 0
+fi
+
+## run until eth_active is false
+while eth_active; do
+    echo "working"
+    sleep 2
+done
+
+## Only kill the PID if the $pid variable is not empty
+[ -n "$pid" ] && kill -9 "$pid"
+
+echo "Ethernet interface is severed. Exiting..."
+blink
+shutdown -h now
+
+#ANOTHER OPTION FOR EXITING SCRIPT
+#test is ethernet is active
+iface=eth0    # Interface name
+while sleep 3; do date; ip link show "$iface" up; echo SS=$?; echo; done
+# Start the packet capture
+dumpcap -i "$iface" -P -w "$(date +'%Y-%m-%d_%H%M').pcap" &
+pid=$!  
+
+# Do nothing until either the network cable is pulled or dumpcap dies
+while eth_active "$iface" && kill -0 "$pid" 2>/dev/null
+do
+    sleep 3
+done
+
+# Kill dumpcap if it's still around
+kill "$pid" 2>/dev/null && ( sleep 3; kill -KILL "$pid" 2>/dev/null ) &
