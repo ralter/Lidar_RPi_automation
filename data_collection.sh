b62b4f91ba955c5a98adf9cf2fcc50df62f58577
lilyjohnston <125685794+lilyjohnston@users.noreply.github.com>
2024-03-04 11:56:51 -0700

    Update data_collection.sh
    latest version as of March 4, 2024 of data_collection script

--- data_collection.sh
+++ data_collection.sh
@@ -1,47 +1,50 @@
 #!/bin/bash
 
 #put in /etc/rc.local
-#a test we can do is edit rc local is echo "hi">~/blank.txt
-# Function to check if the Ethernet interface is active
+
 eth_active() {
-    ip link show "${1:-eth0}" up 2>&1 | grep -qF "state UP"
+    ip link show "$1" 2>&1 | grep "state UP" 2>&1 >/dev/null
 }
 
-lighton = ‘echo 1 > /sys/class/leds/led1/brightness’
-lightoff = ‘echo 0 > /sys/class/leds/led1/brightness’
+lighton = ‘echo 1 | sudo tee /sys/class/leds/led1/brightness’
+lightoff = ‘echo 0 | sudo tee /sys/class/leds/led1/brightness’
+
 blink(){
 max=10
 
 for (( i=1; i <= $max; ++i ))
 do
-    lighton
+    eval $lighton
     echo 'on'
     sleep 0.25
-    lightoff
+    eval $lightoff
     echo 'off'
     sleep 0.25
 done
 }
-
+#mounting USB drive on RPi
+mount -t vfat /dev/sda1 /mnt/usb_mnt -o umask=000
 
 dt=$(date '+%Y_%m_%d_%H_%M')
-umask 0111
+umask 011
 touch $dt.pcap
-
-lightoff
+eval $lightoff
 sleep 5
 
 # Main script
 iface=eth0
 # Start the packet capture
-dumpcap -i "$iface" -P -w "$(date +'%Y-%m-%d_%H%M').pcap" &
+dumpcap -i "$iface" -q -P -w "$dt.pcap" &
 pid=$!  
 
 # Do nothing until either the network cable is pulled or dumpcap dies
-while eth_active "$iface" && kill -0 "$pid" 2>/dev/null
+while eth_active "$iface"
 do
-    sleep 3
+    sleep 1
 done
+echo $pid
+cd
+sudo umount /mnt/usb_mnt
 
 # Kill dumpcap if it's still around
-kill "$pid" 2>/dev/null && ( sleep 3; kill -KILL "$pid" 2>/dev/null ) &
+kill "$pid" && ( sleep 3; kill -KILL "$pid" 2>/dev/null ) 
